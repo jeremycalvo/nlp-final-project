ab06bd41e4e1c1b6638309e6863a7a5fc3db86a0
Tony Bano <tony.bano@epitech.eu>
2024-06-11 16:11:59 +0900

    feat : change text

--- front-nlp/src/app/page.tsx
+++ front-nlp/src/app/page.tsx
@@ -3,7 +3,7 @@ import SearchBar from './components/SearchBar';
 export default function Home() {
   return (
     <div className=" min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-cyan-950 via-sky-500 to-cyan-950">
-      <h1 className="text-white text-3xl mb-8">Search Page</h1>
+      <h1 className="text-white text-3xl mb-8">Search Movies</h1>
       <SearchBar />
     </div>
   );
